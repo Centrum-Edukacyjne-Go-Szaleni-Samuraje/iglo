aed3c9e3808d0c9855c43ab59a224560b4780c4a
konradhalas <halas.konrad@gmail.com>
2021-09-22 15:43:02 +0200

    Set admin page header

--- iglo/iglo/urls.py
+++ iglo/iglo/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+admin.site.site_header = "IGLO Administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'))

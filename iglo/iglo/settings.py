623d601156f380b06ccff17b40c0a7f5a82119f6
konradhalas <halas.konrad@gmail.com>
2021-09-22 15:31:15 +0200

    Add static path config

--- iglo/iglo/settings.py
+++ iglo/iglo/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -15,7 +14,6 @@ SECRET_KEY = 'django-insecure-1-q)d#l=pqgz^9354^2uri96f&lmqb(ieh*$jdvv2$fnhvad%6
 DEBUG = os.environ.get("DEBUG", "true") == "true"
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -58,7 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iglo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -68,7 +65,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3' if DEBUG else BASE_DIR / '../../data/db.sqlite3'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -88,7 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -102,11 +97,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "../../data/static"
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

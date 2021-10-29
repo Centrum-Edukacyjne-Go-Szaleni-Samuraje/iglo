6b3b7635950c661f3c01ebddf8c983edf530c09f
konradhalas <halas.konrad@gmail.com>
2021-10-29 17:15:51 +0200

    Fix deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,4 @@
 cd repo
 git pull
 cd ..
-docker-compose -p iglo-$ENVIRONMENT -f repo/deploy/docker-compose.yml --env-file .env up -d --no-deps --build web
+docker-compose -p iglo-$ENVIRONMENT -f repo/deploy/docker-compose.yml --env-file ./.env up -d --no-deps

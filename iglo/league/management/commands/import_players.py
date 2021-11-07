94a3e27701b8a29dc12b2c2ff6189a57c2be5576
Sebastian Parzych <>
2021-11-07 13:59:07 +0100

    Add import_players command

--- iglo/league/management/commands/import_players.py
+++ iglo/league/management/commands/import_players.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import islice
 from pathlib import Path
 
 from django.contrib.auth.hashers import make_password
@@ -20,31 +21,22 @@ class Command(BaseCommand):
         if not file_path.is_file():
             raise CommandError(f'File {file_path} not found')
 
-        players = self.load_csv(file_path)
-        self.fill_db(players)
-
-    @staticmethod
-    def load_csv(file_path):
         with file_path.open(encoding='utf8') as csv_file:
             reader = csv.DictReader(csv_file, fieldnames=('full_name', 'email', 'nick', 'rank', 'group'))
-            content = list(reader)
-            return content[1:]  # omit header
 
-    @staticmethod
-    def fill_db(players):
-        for player_info in players:
-            print(player_info)
-            user, _ = User.objects.get_or_create(
-                email__iexact=player_info['email'],
-                defaults={
-                    'email': player_info['email'],
-                    'password': make_password(None)
-                })
-            Player.objects.update_or_create(
-                nick__iexact=player_info['nick'],
-                defaults={
-                    'nick': player_info['nick'],
-                    'user': user,
-                    'rank': player_info['rank'] or 1200
-                }
-            )
+            for player_info in islice(reader, 1, None):
+                print(player_info)
+                user, _ = User.objects.get_or_create(
+                    email__iexact=player_info['email'],
+                    defaults={
+                        'email': player_info['email'],
+                        'password': make_password(None)
+                    })
+                Player.objects.update_or_create(
+                    nick__iexact=player_info['nick'],
+                    defaults={
+                        'nick': player_info['nick'],
+                        'user': user,
+                        'rank': player_info['rank'] or 1200
+                    }
+                )

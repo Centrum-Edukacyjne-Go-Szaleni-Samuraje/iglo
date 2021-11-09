845a8d3777e6cc919944d7aa485e96420f566735
konradhalas <halas.konrad@gmail.com>
2021-11-09 22:36:22 +0100

    Improve registration form

--- iglo/accounts/forms.py
+++ iglo/accounts/forms.py
@@ -9,8 +9,8 @@ from league.models import Player
 
 class RegistrationForm(forms.Form):
     nick = forms.SlugField(help_text=texts.NICK_HELP_TEXT)
-    first_name = forms.CharField(label=texts.FIRST_NAME_LABEL)
-    last_name = forms.CharField(label=texts.LAST_NAME_LABEL)
+    first_name = forms.CharField(label=texts.FIRST_NAME_LABEL, help_text=texts.FIRST_NAME_HELP_TEXT)
+    last_name = forms.CharField(label=texts.LAST_NAME_LABEL, help_text=texts.LAST_NAME_HELP_TEXT)
     rank = forms.IntegerField(help_text=texts.RANK_HELP_TEXT)
     email = forms.EmailField(help_text=texts.EMAIL_HELP_TEXT)
     password = forms.CharField(
@@ -18,6 +18,7 @@ class RegistrationForm(forms.Form):
         label=texts.PASSWORD_LABEL,
         help_text=texts.PASSWORD_HELP_TEXT,
     )
+    agreement = forms.BooleanField(label=texts.AGREEMENT_HELP_LABEL, required=True)
 
     def clean_password(self):
         password = self.cleaned_data["password"]
--- iglo/accounts/texts.py
+++ iglo/accounts/texts.py
@@ -1,8 +1,10 @@
 from django.utils.safestring import mark_safe
 
-EMAIL_HELP_TEXT = "Twój adres e-mail będzie widoczny tylko dla administratorów ligi. Będziesz używać go do logowania oraz bedziemy przesyłać na niego powiadomienia."
+EMAIL_HELP_TEXT = "Twój adres e-mail będzie widoczny dla administratorów ligi oraz dla Twoich przeciwników. Będziesz używać go do logowania oraz bedziemy przesyłać na niego powiadomienia."
+FIRST_NAME_HELP_TEXT = "Twoje imie będzie widoczne dla administratorów ligi oraz Twoich przeciwników."
+LAST_NAME_HELP_TEXT = "Twoje nazwisko będzie widoczne dla administratorów ligi oraz Twoich przeciwników."
 PASSWORD_LABEL = "Hasło"
-NICK_HELP_TEXT = "Pod tą nazwą będziesz widoczny dla innych graczy."
+NICK_HELP_TEXT = "Pod tą nazwą będziesz widoczny jako gracz w lidze."
 RANK_HELP_TEXT = mark_safe('Twoja aktualna siła w <a href="#" data-bs-toggle="modal" data-bs-target="#gor-modal">punktach GoR</a>. Jeżeli dopiero zaczynasz przygodę z Go wpisz 100.')
 EMAIL_ERROR = "Ten e-mail jest już zajęty."
 NICK_ERROR = "Ten nick jest już zajęty."
@@ -10,3 +12,4 @@ REGISTRATION_SUCCESS = "Konto zostało utworzone. Możesz się zalogować."
 PASSWORD_HELP_TEXT = "Wybierz trudne hasło oraz składające się z conajmniej 8 znaków."
 FIRST_NAME_LABEL = "Imie"
 LAST_NAME_LABEL = "Nazwisko"
+AGREEMENT_HELP_LABEL = mark_safe("Zapoznałem się i zgadzam się z <a href='/rules'>regulaminem ligi</a>")

e0119960c5fed0e9eb72cb0a3df4f1984245b321
konradhalas <halas.konrad@gmail.com>
2021-11-10 16:38:40 +0100

    Small password/email change improvement

--- iglo/accounts/urls.py
+++ iglo/accounts/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path("registration", RegistrationView.as_view(), name="registration"),
     path("login", LoginView.as_view(), name="login"),
     path("logout", LogoutView.as_view(), name="logout"),
-    path("password_change", PasswordAndEmailChangeView.as_view(), name="password_change"),
-    path("password_reset/", PasswordResetView.as_view(), name='password_reset'),
-    path("password_reset/done/", PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path("account", PasswordAndEmailChangeView.as_view(), name="password_change"),
+    path("password-reset/", PasswordResetView.as_view(), name='password_reset'),
+    path("password-reset/done/", PasswordResetDoneView.as_view(), name='password_reset_done'),
     path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path("reset/done/", PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]

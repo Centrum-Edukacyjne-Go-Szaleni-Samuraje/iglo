f34165eec86a3f96f4b8429082cf25621e4b1fe1
konradhalas <halas.konrad@gmail.com>
2021-11-12 12:03:13 +0100

    Improve admin for user/player

--- iglo/accounts/admin.py
+++ iglo/accounts/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from accounts.models import User
 
-admin.site.register(User)
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ["email", "last_login", "has_password"]
+    search_fields = ["email"]
+
+    def has_password(self, object):
+        return object.has_usable_password()
+
+    has_password.boolean = True
+
+
+admin.site.register(User, UserModelAdmin)
--- iglo/league/admin.py
+++ iglo/league/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 from league.models import Season, Group, Player, Game, Round
 
+
+class PlayerModelAdmin(admin.ModelAdmin):
+    list_display = ["nick", "first_name", "last_name", "rank", "user"]
+    search_fields = ["nick", "first_name", "last_name", "user__email"]
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related("user")
+
+
 admin.site.register(Season)
 admin.site.register(Group)
-admin.site.register(Player)
+admin.site.register(Player, PlayerModelAdmin)
 admin.site.register(Game)
 admin.site.register(Round)

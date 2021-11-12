4769c30f24c139d6da200a3c7a0e83d265fe200b
konradhalas <halas.konrad@gmail.com>
2021-11-12 17:12:59 +0100

    Add more uwsgi processes

--- deploy/web.sh
+++ deploy/web.sh
@@ -3,4 +3,4 @@
 ../deploy/wait-for-it.sh -t 60 db:5432
 ./manage.py collectstatic --noinput
 ./manage.py migrate --noinput
-uwsgi --http :$VIRTUAL_PORT --module iglo.wsgi --static-map /static=/data/static --static-map /media=/data/media
+uwsgi --http :$VIRTUAL_PORT --module iglo.wsgi --static-map /static=/data/static --static-map /media=/data/media --master --processes 4

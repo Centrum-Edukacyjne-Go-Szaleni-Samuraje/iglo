4bcd113f9ae9186663fb147b396dfb09820e31c6
konradhalas <halas.konrad@gmail.com>
2021-11-12 18:07:06 +0100

    Fix season number in paths

--- iglo/league/urls.py
+++ iglo/league/urls.py
@@ -5,11 +5,11 @@ from league.views import SeasonsListView, SeasonDetailView, GroupDetailView, Gam
 urlpatterns = [
     path('seasons', SeasonsListView.as_view(), name="seasons-list"),
     path('seasons/prepare', PrepareSeasonView.as_view(), name="seasons-prepare"),
-    path('seasons/<number>', SeasonDetailView.as_view(), name="season-detail"),
-    path('seasons/<season_number>/<group_name>', GroupDetailView.as_view(), name="group-detail"),
-    path('seasons/<season_number>/<group_name>/<black_player>-<white_player>', GameDetailView.as_view(),
+    path('seasons/<int:number>', SeasonDetailView.as_view(), name="season-detail"),
+    path('seasons/<int:season_number>/<group_name>', GroupDetailView.as_view(), name="group-detail"),
+    path('seasons/<int:season_number>/<group_name>/<black_player>-<white_player>', GameDetailView.as_view(),
          name="game-detail"),
-    path('seasons/<season_number>/<group_name>/<black_player>-<white_player>/edit', GameUpdateView.as_view(),
+    path('seasons/<int:season_number>/<group_name>/<black_player>-<white_player>/edit', GameUpdateView.as_view(),
          name="game-update"),
     path('players/<slug>', PlayerDetailView.as_view(), name="player-detail"),
     path('players/<slug>/settings', PlayerUpdateView.as_view(), name="player-settings")

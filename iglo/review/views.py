615a5bed3526e03bd70b05d5cf057d46f19c092c
konradhalas <halas.konrad@gmail.com>
2022-01-23 14:36:42 +0100

    Sort by first and last name

--- iglo/review/views.py
+++ iglo/review/views.py
@@ -7,6 +7,9 @@ from review.models import Teacher
 class TeacherListView(ListView):
     model = Teacher
 
+    def get_queryset(self):
+        return super().get_queryset().order_by("first_name", "last_name")
+
 
 class TeacherDetailView(DetailView):
     model = Teacher

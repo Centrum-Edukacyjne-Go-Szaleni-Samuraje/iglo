02d4f9697aa7d151b636f279c4fbcab770e3a2f1
konradhalas <halas.konrad@gmail.com>
2022-02-14 11:45:54 +0100

    Add build worker step

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker-compose -p iglo-$ENVIRONMENT -f repo/deploy/docker-compose.yml --env-file ./.env up -d --no-deps --build web
+docker-compose -p iglo-$ENVIRONMENT -f repo/deploy/docker-compose.yml --env-file ./.env up -d --no-deps --build worker

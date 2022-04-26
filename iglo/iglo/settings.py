ccdc375bb4b48a366494fcfb83575ac8c97795e7
konradhalas <halas.konrad@gmail.com>
2022-04-26 16:39:18 +0200

    Use translated urls instead of cookie

--- iglo/iglo/settings.py
+++ iglo/iglo/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "bootstrap_pagination",
     "django_countries",
+    "utils",
 ]
 
 MIDDLEWARE = [
--- iglo/iglo/urls.py
+++ iglo/iglo/urls.py
@@ -1,17 +1,22 @@
 import debug_toolbar
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 admin.site.site_header = "IGLO Administration"
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
+localized_patterns = i18n_patterns(
     path("", include("league.urls")),
     path("", include("misc.urls")),
     path("", include("accounts.urls", namespace="accounts")),
     path("", include("review.urls")),
+    prefix_default_language=False,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + localized_patterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

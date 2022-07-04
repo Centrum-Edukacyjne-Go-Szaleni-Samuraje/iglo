5e148bc8afa7a2380e2fc5f9d00c53e15514197a
konradhalas <halas.konrad@gmail.com>
2022-07-04 15:34:15 +0200

    Handle missing request in context

--- iglo/utils/templatetags/translate_url.py
+++ iglo/utils/templatetags/translate_url.py
@@ -4,5 +4,8 @@ from django.urls import translate_url as django_translate_url
 
 @register.simple_tag(takes_context=True)
 def translate_url(context, lang_code):
-    path = context.get('request').get_full_path()
-    return django_translate_url(path, lang_code)
+    try:
+        path = context["request"].get_full_path()
+        return django_translate_url(path, lang_code)
+    except KeyError:
+        return django_translate_url("/", lang_code)

561bda7aa345a19ec2d50443acbcf08d195a11c0
Lukasz Lew <lukasz.lew@gmail.com>
2023-05-07 00:42:29 -0700

    Update toml

--- iglo/iglo/settings.py
+++ iglo/iglo/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = "iglo.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default="postgres://postgres:postgres@localhost:5432/postgres"),
+    "default": dj_database_url.config(default="postgres://postgres:postgres@localhost:15432/postgres"),
 }
 
 # Password validation
